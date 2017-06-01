e14dab36a1e950401dd59f5d19a0575cc51f775a
maaaaagda <magdapolak96@gmail.com>
2017-06-02 00:35:24 +0200

    with second layer

--- predict.py
+++ predict.py
@@ -52,8 +52,8 @@ data = load_data()
 X_train = data[0]
 Y_train = data[1]
 length = len(X_train)
-cut_off = np.round(np.shape(X_train)[0]*0.05)        #30000
-cut_off_2 = np.round(np.shape(X_train)[1] * 0.05)         #3000
+cut_off = np.round(np.shape(X_train)[0]*0.07)        #30000
+cut_off_2 = np.round(np.shape(X_train)[1]*0.20)         #3000
 X_train = X_train[15000-cut_off: 15000+cut_off, 1500-cut_off_2:1500+cut_off_2]
 Y_train = Y_train[15000-cut_off: 15000+cut_off, :]
 
@@ -64,33 +64,38 @@ g = np.reshape(Y_train, np.shape(Y_train)[0], 1)
 X = X_train
 y = one_hot(g)
 
+
+def lfv(x, s):
+    return x * np.log(s) + (1 - x) * np.log(1 - s)
+
 np.random.seed(1)
+syn0 = np.random.random((cut_off_2 * 2, 36)) - 1
+syn1 = np.random.random((36, 36)) - 1
+for i in range(100):
+        l0 = X
+        l1 = sigma(np.dot(l0, syn0))
+        l2 = sigma(np.dot(l1, syn1))
+        l2_error = y - l2
+        l2_delta = l2_error * sigma(l2, deriv=True)
 
-syn0 = 2 * np.random.random((cut_off_2*2, 36)) - 1
+        l1_error = l2_delta.dot(syn1.T)
+        l1_delta = l1_error * sigma(l1, True)
+        syn1 -= l1.T.dot(l2_delta)
+        syn0 -= np.dot(l0.T, l1_delta)
 
-for i in range(1000):
-    l0 = X
-    l1 = sigma(np.dot(l0, syn0))
-    """print("syn0")
-    print(syn0)
-    print("l1")
-    print(l1)"""
-    l1_error = y - l1
-    l1_delta = l1_error * sigma(l1, True)
-    syn0 += np.dot(l0.T, l1_delta)
+
 
 
 
 print("hej")
 result = np.round(l1)
-b  = np.argmax(l1, axis=1)
-#print(result)
+b = np.argmax(l1, axis=1)
 
 def prediction(predicted, real):
     return np.sum(predicted*real)/np.shape((predicted)[0])
 
-
-print(np.sum(b==g))
+print(len(b))
+print(np.sum(b==g)/len(b))
 print(b)
 print(g)
 print(time.time()-start_time)
